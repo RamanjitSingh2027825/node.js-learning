ba86eaa28a66e65551ae224719c12935b50414e2
Ramanjit Singh <142193780+RamanjitSingh2027825@users.noreply.github.com>
2024-04-10 10:50:14 +0000

    Add index.js to compute sum of two numbers

---
+++ index.js
@@ -0,0 +1,6 @@
+let num1 = parseInt(process.argv[2]) ;
+let num2 = parseInt(process.argv[3]) ;
+let result = num1 + num2 ;
+console.log(`The Sum of ${num1} and ${num2} is ${result}`) ;
+
+// console.log(process.argv) ;
